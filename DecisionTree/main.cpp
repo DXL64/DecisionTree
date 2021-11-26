7f88971b5017bb003078cc3bd62ced0ed890abf5
Loc Dang <locybdxlyb204@gmail.com>
2021-11-26 09:50:47 +0700

    Final

--- DecisionTree/main.cpp
+++ DecisionTree/main.cpp
@@ -2,17 +2,20 @@
 
 using namespace std;
 
-const int maxDepth = 4;
-const int minSize = 1;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+const int minSize = 2;
+const int maxDepth = 6;
 const vector<char> s = {'L', 'R', 'B'};
-const int numberOfTree = 3;
+const int numberOfTree = 30;
+ifstream inFile1, inFile2;
+ofstream outFile;
 const vector<vector<int> > combination
-{ {1,2}, {1,3}, {1,4}, {2,3}, {2,4}, {3,4},
+{ {1}, {2}, {3}, {4},
+  {1,2}, {1,3}, {1,4}, {2,3}, {2,4}, {3,4},
   {1,2,3}, {1,2,4}, {2,3,4},
   {1,2,3,4}
 };
-int indexTree = 9;
-vector< vector<char> > ans;
+int indexTree = 13;
 struct Data {
     char y;
     float x[5];
@@ -34,7 +37,9 @@ struct Tree
     Node* root = new Node;
 };
 vector<Data> trainSet, validSet;
-Tree T[numberOfTree+1], TrainSet;
+vector< vector<char> > ans;
+Tree T[numberOfTree+1];
+Tree TrainSet;
 
 Data read(string s) {
     Data tmp;
@@ -66,13 +71,13 @@ int calculate_Number_Of_Class_In_Group(char _class, vector<Data> _group) {
     return cnt;
 }
 
-float getGiniScore(vector< vector<Data> > groups, vector<char> classes) {
+float getGiniScore(vector<vector<Data> > *groups, vector<char> classes) {
     int n = 0;
-    for(auto i : groups) {
+    for(auto i : *groups) {
         n = n + i.size();
     }
     float gini = 0.0;
-    for(auto group : groups) {
+    for(auto group : *groups) {
         int _size = group.size();
         if(!_size) continue;
         float score = 0.0;
@@ -84,10 +89,10 @@ float getGiniScore(vector< vector<Data> > groups, vector<char> classes) {
     }
     return gini;
 }
-vector< vector<Data> > splitGroups(int index, int value, vector<Data> dataSet) {
+vector< vector<Data> > splitGroups(int index, int value, vector<Data>* dataSet) {
     vector< vector<Data> > g;
     vector<Data> g1, g2;
-    for(auto i : dataSet) {
+    for(auto i : *dataSet) {
         if(i.x[index] < value)
             g1.push_back(i);
         else
@@ -98,17 +103,17 @@ vector< vector<Data> > splitGroups(int index, int value, vector<Data> dataSet) {
     return g;
 }
 
-nodeInfo getSplit(vector<Data> dataSet, Node* Node, vector<Data> &left, vector<Data> &right) {
+nodeInfo getSplit(vector<Data>* dataSet, Node* Node, vector<Data> &left, vector<Data> &right) {
     float minGini = 1.0;
     nodeInfo tmp;
     for(auto index : combination[indexTree]) {
-        for(auto row : dataSet) {
-            vector< vector<Data> > groups = splitGroups(index, row.x[index], dataSet);
-            float  gini = getGiniScore(groups, s);
+        for(int row = 1; row <= 5; row++) {
+            vector< vector<Data> > groups = splitGroups(index, row, dataSet);
+            float  gini = getGiniScore(&groups, s);
             if(gini < minGini) {
                 minGini = gini;
                 tmp.id = index;
-                tmp.value = row.x[index];
+                tmp.value = row;
                 left = groups[0];
                 right = groups[1];
             }
@@ -117,9 +122,9 @@ nodeInfo getSplit(vector<Data> dataSet, Node* Node, vector<Data> &left, vector<D
     return tmp;
 }
 
-char toTerminal(vector<Data> group) {
+char toTerminal(vector<Data>* group) {
     int cntL = 0, cntR = 0, cntB = 0;
-    for(auto itr : group)
+    for(auto itr : *group)
     {
         if(itr.y == 'L') cntL++;
         if(itr.y == 'R') cntR++;
@@ -130,18 +135,17 @@ char toTerminal(vector<Data> group) {
     if(max(cntL, max(cntR, cntB)) == cntB) return 'B';
 }
 
-void splitTree(Node* Node, vector<Data> dataSet, int depth) {
+void splitTree(Node* Node, vector<Data>* dataSet, int depth) {
     (Node->left) = new struct Node;
     Node->right = new struct Node;
     vector<Data> left;
     vector<Data> right;
     Node->nodeInfo = getSplit(dataSet, Node, left, right);
-//    cout << left.size() << " " << right.size() << "\n" ;
     if(left.size() == 0 || right.size() == 0) {
         vector<Data> v;
         v.insert(v.begin(), left.begin(), left.end());
         v.insert(v.end(), right.begin(), right.end());
-        char tmp = toTerminal(v);
+        char tmp = toTerminal(&v);
         ((Node->right))->ans = tmp;
         ((Node->right)->nodeInfo).isLeaf = 1;
         ((Node->left))->ans = tmp;
@@ -152,34 +156,32 @@ void splitTree(Node* Node, vector<Data> dataSet, int depth) {
         return ;
     }
     if(depth >= maxDepth) {
-        (Node->left)->ans = toTerminal(left);
+        (Node->left)->ans = toTerminal(&left);
         ((Node->left)->nodeInfo).isLeaf = 1;
-        (Node->right)->ans = toTerminal(right);
+        (Node->right)->ans = toTerminal(&right);
         ((Node->right)->nodeInfo).isLeaf = 1;
         left.clear();
         right.clear();
         return;
     }
     if(left.size() <= minSize) {
-        (Node->left)->ans = toTerminal(left);
+        (Node->left)->ans = toTerminal(&left);
         ((Node->left)->nodeInfo).isLeaf = 1;
     }
     else{
-        splitTree(Node->left, left, depth+1);
+        splitTree(Node->left, &left, depth+1);
     }
     if(right.size() <= minSize) {
-        (Node->right)->ans = toTerminal(right);
+        (Node->right)->ans = toTerminal(&right);
         ((Node->right)->nodeInfo).isLeaf = 1;
     }
     else{
-        splitTree(Node->right, right, depth+1);
+        splitTree(Node->right, &right, depth+1);
     }
     return;
 }
 void printTree(Node* Node, int depth) {
-//    cout << (Node->nodeInfo).dataSet.size() << "\n";
     if(depth > maxDepth || Node == NULL || (Node->nodeInfo).isLeaf) return;
-//    cout << depth << " " << Node << " " << Node->left << " " << Node->right << "\n" ;
     if(Node->left != NULL)
         printTree(Node->left, depth+1);
     if(Node->right != NULL)
@@ -199,76 +201,70 @@ char predict(Node* Node, Data row) {
             return predict(Node->right, row);
     }
 }
-void buildTree(Node* root, vector<Data> _trainSet) {
+void buildTree(Node* root, vector<Data> *_trainSet) {
     splitTree(root, _trainSet, 1);
-//    printTree(root, 1);
+
     int cnt = 0;
     for(auto test : validSet) {
         char tmp = predict(root, test);
         ans[cnt].push_back(tmp);
-        if(indexTree == 9)
-        {
-            ans[cnt].push_back(tmp);
-        }
         cnt++;
-//        if(predict(root, test) == test.y) cnt++;
     }
-
-//    cout << "Percent: " << (float)cnt / validSet.size() * 100 << "%";
 }
 void RandomData(vector<Data> &trainSample) {
     int trainSize = trainSet.size();
-    srand(time(NULL));
-    for(int i = 0; i < trainSize; i++)
+    for(int i = 0; i < 80; i++)
     {
-        int index = rand() % trainSize;
+        int index = (rng() * rng()) % 35 + 405;
         trainSample.push_back(trainSet[index]);
     }
-    indexTree = rand()%9;
-//    indexTree--;
-
+    for(int i = 80; i < trainSize; i++)
+    {
+        int index = (rng() * rng()) % trainSize;
+        trainSample.push_back(trainSet[index]);
+    }
+    int random = (rng() * rng())%14;
+    indexTree = random;
 }
 void RandomForest() {
     vector<Data> trainSample;
     for(int i = 1; i <= numberOfTree; i++) {
         RandomData(trainSample);
-        buildTree(T[i].root, trainSample);
+        buildTree(T[i].root, &trainSample);
     }
 }
 void printAns() {
-    int cnt[100];
-    int accurancy = 0;
+    int cnt[200];
+    int accuracy = 0;
     for(int i = 0; i < validSet.size(); i++) {
         cnt['L'] = 0;
         cnt['R'] = 0;
         cnt['B'] = 0;
-        cout << validSet[i].y << " ";
+//        outFile << validSet[i].y << " ";
+//        if(validSet[i].y == ans[i][0]) accuracy++;
         for(auto tmp : ans[i])
-        {
             cnt[tmp]++;
-            cout << tmp << " ";
-        }
-        cout << "\n";
-        if(cnt['L'] >= cnt['R'] && cnt['L'] >= cnt['B'])
+        if(cnt['L'] >= cnt['R'] && cnt['L'] >= cnt['B']) {
             if('L' == validSet[i].y)
-                accurancy++;
-        if(cnt['R'] >= cnt['L'] && cnt['R'] >= cnt['B'])
+                accuracy++;
+        }
+        else if(cnt['R'] >= cnt['L'] && cnt['R'] >= cnt['B']) {
             if('R' == validSet[i].y)
-                accurancy++;
-        if(cnt['B'] >= cnt['R'] && cnt['B'] >= cnt['B'])
+                accuracy++;
+        }
+        else if(cnt['B'] >= cnt['R'] && cnt['B'] >= cnt['L']) {
             if('B' == validSet[i].y)
-                accurancy++;
+                accuracy++;
+        }
     }
-    cout << "Accurancy :" << (float)accurancy / validSet.size() * 100 << "%";
+    outFile << (float)accuracy / validSet.size() * 100 << "\n" ;
 }
 int main()
 {
-    ifstream inFile1, inFile2;
-    ofstream outFile;
-    string s;
     outFile.open("test.txt");
     inFile1.open("train.txt", ios::in);
     inFile2.open("valid.txt", ios::in);
+    string s;
     while(getline(inFile1, s)) {
         Data inp = read(s);
         trainSet.push_back(inp);
@@ -278,8 +274,8 @@ int main()
         validSet.push_back(inp);
     }
     for(int i = 0; i < validSet.size(); i++)
-    ans.push_back(vector<char>());
-    buildTree(TrainSet.root, trainSet);
+        ans.push_back(vector<char>());
+    buildTree(TrainSet.root, &trainSet);
     RandomForest();
     printAns();
     return 0;
